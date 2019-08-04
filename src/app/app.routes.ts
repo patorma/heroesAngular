86513f387a23675816872e857528098b68e26d24
patorma <pocontreras@ing.ucsc.cl>
2019-08-04 18:59:56 -0400

    se creo componentes de heroes y la aplicacion quedo lista para que un servicio muestre la mayoria de heroes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,27 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
+const routes: Routes = [
+  /*   { path: '', component: HomeComponent }, */
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'heroes', component: HeroesComponent },
+    { path: '**',pathMatch:'full',redirectTo: 'home' },
+    //si no existe las anteriores rutas ejecuta la ultima que tiene **
+
+    //{ path: 'path/:routeParam', component: MyComponent },
+    //{ path: 'staticPath', component: ... },
+    //{ path: '**', component: ... },
+    //{ path: 'oldPath', redirectTo: '/staticPath' },
+    //{ path: ..., component: ..., data: { message: 'Custom' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes,{useHash:true})],
+    exports: [RouterModule]
+})
+export class FeatureRoutingModule {}
